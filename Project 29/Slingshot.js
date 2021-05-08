f02ff5dc9822bd10a0eda5ca74bea711fc34cff1
Patil5115 <75479605+Patil5115@users.noreply.github.com>
2021-05-08 23:13:58 +0530

    Add files via upload

---
+++ Project 29/Slingshot.js
@@ -0,0 +1,30 @@
+Class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+
+    }
+
+    display(){
+       if(this.sling.bodyA){
+    
+        var pointA = this.polygon_img.bodyA.position;
+        var pointB = this.pointB;   
+        
+        
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
+}
